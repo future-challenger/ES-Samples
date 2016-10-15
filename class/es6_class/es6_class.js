d2e894a7af749b8ff26f52bd29060614e20d61f8
Sharper <sharp.bruce.lee@gmail.com>
2016-10-15 22:46:03 +0800

    inheritance - static methods

--- class/es6_class/es6_class.js
+++ class/es6_class/es6_class.js
@@ -1,13 +1,18 @@
 //@flow
 
+var _bookNameSymbol = Symbol();
+
 class ES6Detective {
   detectiveName: string;
   _bookName: string;
+  // this[_bookNameSymbol]: string;
 
   constructor() {
     console.log('Detective constructor');
     this.detectiveName = 'Detective who';
     this._bookName = 'who';
+
+    this[_bookNameSymbol] = 'who symbol';
   }
 
   solveCase(caseName) {
@@ -30,8 +35,9 @@ class ES6Detective {
     return 'Author Who';
   }
 
-  static countCases() {
-    console.log(`Counting cases...`);
+  static countCases(place) {
+    let p = !place ? '[maybe]' : place;
+    console.log(`Counting cases...solve in ${p}`);
   }
 }
 
@@ -40,20 +46,40 @@ class ES6DetectiveConan extends ES6Detective {
     super();
     console.log('ES6DetectiveConan constructor');
   }
+
+  solveCase(caseName) {
+    super.solveCase(caseName);
+
+    if(!caseName) {
+      console.log('CONAN no case to solve');
+    } else {
+      console.log('CONAN case ' + caseName + ' is solved');
+    }
+  }
+
+  static countCases(place) {
+    let p = !place ? '[maybe]' : place;
+    super.countCases(p);
+    console.log(`#Sub class:- Counting cases...solve in ${p}`);
+  }
 }
 
 let detective = new ES6Detective();
 console.log(`${detective.detectiveName}`);
 console.log(`##original book name:- ${detective.fromBookName}`);
 console.log(`##inner original book name:- ${detective.fromBookName}`);
+console.log(`##Symbol book name:- ${detective[_bookNameSymbol]}`);
 
 detective.fromBookName = 'Detective Conan';
-detective.bookAuthor = 'A C';
+// detective.bookAuthor = 'A C'; // give a value to only a getter attribute.
 
 let conan = new ES6DetectiveConan();
+conan.solveCase('Some case');
+
 console.log(`${conan.detectiveName}`);
 console.log(`##conan book name:- ${conan.fromBookName}`);
 console.log(`##inner conan book name:- ${conan.fromBookName}`);
 
 // static method
 ES6Detective.countCases();
+ES6DetectiveConan.countCases('Japan');
